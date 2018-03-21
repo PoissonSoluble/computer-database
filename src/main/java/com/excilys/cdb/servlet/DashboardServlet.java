38549b7e38ce363da5a30cf8984bb3c0756c2a2a
vduplessis <vduplessis@excilys.com>
2018-03-21 11:37:04 +0100

    [FEATURE](web_ui) first page printed on dashboard

--- src/main/java/com/excilys/cdb/servlet/DashboardServlet.java
+++ src/main/java/com/excilys/cdb/servlet/DashboardServlet.java
@@ -1,6 +1,8 @@
 package com.excilys.cdb.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.excilys.cdb.dto.ComputerDTO;
+import com.excilys.cdb.mapper.ComputerDTOMapper;
+import com.excilys.cdb.model.Computer;
 import com.excilys.cdb.pagination.ComputerPage;
 
 public class DashboardServlet extends HttpServlet {
@@ -19,7 +24,14 @@ public class DashboardServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("computer", new ComputerPage(1, 10));
+        List<Computer> computers = new ComputerPage(2, 10).get();
+        List<ComputerDTO> dtos = new ArrayList<>();
+        ComputerDTOMapper mapper = new ComputerDTOMapper();
+        computers.forEach(computer -> dtos.add(mapper.createComputerDTO(computer)));
+        for(ComputerDTO dto : dtos) {
+            System.out.println(dto.getName());
+        }
+        request.setAttribute("computers", dtos);
         RequestDispatcher view = request.getRequestDispatcher("dashboard.jsp");
         view.forward(request, response);  
     }
